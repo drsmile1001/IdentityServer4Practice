73b3a8bcfbb18f558a41a96bdb8e7e7eb0b793fe
drsmile <drsmile.1001@gmail.com>
2019-09-04 13:10:42 +0800

    加入console client 與 受保護的aspnet core api 實踐Client Credentials

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,61 @@
+﻿using IdentityModel.Client;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await ClientCredentials();
+
+            Console.ReadKey();
+        }
+
+        private static async Task ClientCredentials()
+        {
+            // discover endpoints from metadata
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            // request token
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+
+                ClientId = "client",
+                ClientSecret = "511536EF-F270-4058-80CA-1C89C192F69A",
+                Scope = "api1"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+
+            // call api
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await client.GetAsync("http://localhost:5002/api/values");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(content);
+            }
+        }
+    }
+}
